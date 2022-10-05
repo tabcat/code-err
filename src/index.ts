6491c855d2bd7dda7d5a166f40c4407e64bdad7e
tabcat <tabcat00@proton.me>
2022-10-05 13:44:54 -0500

    move index.ts to src

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 // https://github.com/libp2p/js-libp2p/issues/1269#issue-1280763716
-
 export default class CodeError<T extends Record<string, any>> extends Error {
   code: string
   props: T
 
-  constructor(message: string, code: string, props?: T) {
+  constructor (message: string, code: string, props?: T) {
     super(message)
 
     this.code = code
-    this.props = props ?? {} as T
+    this.props = props ?? {} as T // eslint-disable-line @typescript-eslint/consistent-type-assertions
   }
 }
