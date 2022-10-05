4759cf2ee03c1c5ef54f95ca5f221c415572a7c9
tabcat <tabcat00@proton.me>
2022-10-05 11:42:26 -0500

    add index.ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+// https://github.com/libp2p/js-libp2p/issues/1269#issue-1280763716
+
+export default class CodeError<T extends Record<string, any>> extends Error {
+  code: string
+  props: T
+
+  constructor(message: string, code: string, props?: T) {
+    super(message)
+
+    this.code = code
+    this.props = props ?? {} as T
+  }
+}
