4ea290a0ac12d07ec9fbeb977d108ce03566beca
tabcat <tabcat00@proton.me>
2023-01-12 22:22:12 -0600

    refactor!: change api/add more options
    - api matches Error(message, options)
    - test code empty string
    - add name option
    - test name option
    
    Closes #5
    
    - Decided not to make the class callable like Error

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
+export interface Options<T extends Record<string, any>> {
+  cause?: unknown
+  name?: string
+  code?: string
+  props?: T
+}
+
 // https://github.com/libp2p/js-libp2p/issues/1269#issue-1280763716
 export class CodeError<T extends Record<string, any>> extends Error {
-  constructor (
-    message: string,
-    public readonly code: string,
-    public readonly props: T = {} as T // eslint-disable-line @typescript-eslint/consistent-type-assertions
-  ) {
-    super(message)
+  public readonly code: string
+  public readonly props: T
 
-    this.name = 'CodeError'
+  constructor (message?: string, options?: Options<T>) {
+    super(message, options)
+
+    this.name = options?.name ?? 'CodeError'
+    this.code = options?.code ?? ''
+    this.props = options?.props ?? ({} as T) // eslint-disable-line @typescript-eslint/consistent-type-assertions
   }
 }
--- test/test.ts
+++ test/test.ts
@@ -14,8 +14,20 @@ describe('code-err', () => {
     bar: 'foo'
   }
 
+  it('should work fine without a message or code', () => {
+    const empty = ''
+    const err = new CodeError()
+
+    expect(err).to.be.an.instanceof(CodeError)
+    expect(err).to.be.an.instanceof(Error)
+    expect(err.message).to.be.equal(empty)
+    expect(err.name).to.be.equal(name)
+    expect(err.code).to.be.equal(empty)
+    expect(err.props).to.be.deep.equal({})
+  })
+
   it('should accept a message and add a code', () => {
-    const err = new CodeError(message, code)
+    const err = new CodeError(message, { code })
 
     expect(err).to.be.an.instanceof(CodeError)
     expect(err).to.be.an.instanceof(Error)
@@ -26,7 +38,7 @@ describe('code-err', () => {
   })
 
   it('should accept a message and add a code & properties', () => {
-    const err = new CodeError<Props>(message, code, props)
+    const err = new CodeError<Props>(message, { code, props })
 
     expect(err).to.be.an.instanceof(CodeError)
     expect(err).to.be.an.instanceof(Error)
@@ -35,5 +47,17 @@ describe('code-err', () => {
     expect(err.code).to.be.equal(code)
     expect(err.props.foo).to.be.equal(props.foo)
     expect(err.props.bar).to.be.equal(props.bar)
+  })
+
+  it('should accept and set a name from options', () => {
+    const name = 'CustomName'
+    const err = new CodeError(message, { code, name })
+
+    expect(err).to.be.an.instanceof(CodeError)
+    expect(err).to.be.an.instanceof(Error)
+    expect(err.message).to.be.equal(message)
+    expect(err.name).to.be.equal(name)
+    expect(err.code).to.be.equal(code)
+    expect(err.props).to.be.deep.equal({})
   })
 })
